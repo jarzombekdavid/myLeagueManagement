87d6b94705fdfa93bbbbecad1caa9b4565b41ffe
david <jarzombekdavid@gmail.com>
2018-06-10 11:19:11 -0400

    form update
    form update

--- myLeague/mlm/forms.py
+++ myLeague/mlm/forms.py
@@ -1,7 +1,14 @@
 from django.forms import ModelForm
 from mlm.models import Season
+from mlm.models import Team
 
 
 class SeasonForm(ModelForm):
 	class meta:
 		model = Season
+		fields = ['name','draft_date']
+
+class TeamForm(ModelForm):
+	class meta:
+		model = Team
+		fields = ['name','Season','user1','user2','multi_user']
--- myLeague/mlm/views.py
+++ myLeague/mlm/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
-
-# Create your views here.
-
+from models import Season
+from models import SeasonForm
+from django.forms import modelformset_factory
 from django.http import HttpResponse
 
 
 def index(request):
+
     return HttpResponse("Hello, world. You're at the 2kleague.")
 
+
+def seasons(request):
+
+	season_list = Season.objects.all()
+
+
+def create_season(request):
+	
+	SeasonFormSet = modelformset_factory(Season, fields=('name', 'draft_date'))
+    if request.method == 'POST':
+        formset = SeasonFormSet(request.POST, request.FILES)
+        if formset.is_valid():
+            formset.save()
+            # do something.
+    else:
+        formset = SeasonFormSet()
+    return render(request, 'manage_authors.html', {'formset': formset})
